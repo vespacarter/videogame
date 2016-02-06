33561a2e24efb9c3b72f579b52e8b11f5abbae87
vespacarter <skasez@gmail.com>
2016-02-06 17:13:18 +0100

    third iteration completed

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -1,6 +1,19 @@
 class Api::TournamentsController < ApplicationController
+
   def index
     tournaments = Tournament.all
     render json: tournaments
   end
+
+  def create
+  	tournament = Tournament.create(tournament_params)
+  	render json: tournament, status: 201
+  end
+
+  private
+
+  	def tournament_params
+    	params.require(:tournament).permit(:name)
+  	end
+
 end
